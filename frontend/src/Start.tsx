d0acc819626d1fdf314caf711b22a2795f1cc999
Alexander Erkhov <sasori654654@gmail.com>
2025-11-29 16:25:17 +0300

    first part of start page

--- frontend/src/Start.tsx
+++ frontend/src/Start.tsx
@@ -1,9 +1,22 @@
-
+import { Link } from 'react-router-dom';
 
 function Start() {
   return (
-    <div>Hello World</div>
-  )
+    <div className="main-page">
+      <div className="w-[450px] h-[260px] rounded-lg bg-white shadow-lg">
+        <Link to="/login">
+          <div className="w-[410px] h-[100px] m-5 rounded-lg spbu flex items-center justify-center">
+            <span className="text-white text-2xl">Войти</span>
+          </div>
+        </Link>
+        <Link to="/register">
+          <div className="w-[410px] h-[100px] m-5 rounded-lg spbu flex items-center justify-center">
+            <span className="text-white text-2xl">Зарегистрироваться</span>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default Start
+export default Start;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,10 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom"
 import Start from './Start.tsx'
 
 const router = createBrowserRouter([
-    { path: "/", element: <Start /> }])
+    { path: "/", element: <Start /> },
+    { path: "/login", element: <div>Login Page</div> },
+    { path: "/register", element: <div>Register Page</div> }
+])
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
