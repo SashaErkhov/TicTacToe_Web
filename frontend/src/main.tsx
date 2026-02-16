db988a975174b29409b5e71c2ba13dadfc884db9
Ibragim Oskanov <oskanov-ibragim.06@mail.ru>
2026-02-16 19:56:16 +0300

    Use AuthContext, update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,27 +1,55 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
 import Start from './pages/Start.tsx'
 import Login from './pages/Login.tsx'
 import Register from "./pages/Register.tsx";
 import New from "./pages/New.tsx";
 import Profile from "./pages/Profile.tsx";
 import Leaders from "./pages/Leaders.tsx";
-import Match from "./pages/Match.tsx";
-
-const router = createBrowserRouter([
-    { path: "/", element: <Start /> },
-    { path: "/login", element: <Login /> },
-    { path: "/register", element: <Register /> },
-    { path: "/new", element: <New /> },
-    { path: "/profile", element: <Profile /> },
-    { path: "/leaders", element: <Leaders /> },
-    { path: "/matches/:matchId", element: <Match />, }
-])
+import Match from "./pages/Match.tsx"; // Online game
+import MatchHotseat from "./pages/MatchHotseat.tsx";
+import {AuthProvider} from "./AuthContext.tsx";
+import ProtectedRoute from "./ProtectedRoute.tsx";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <RouterProvider router={router} />
+        <BrowserRouter>
+            <AuthProvider>
+                <Routes>
+                    <Route path="/" element={<Start />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+
+                    <Route path="/new" element={
+                        <ProtectedRoute>
+                            <New />
+                        </ProtectedRoute>
+                    } />
+                    <Route path="/profile" element={
+                        <ProtectedRoute>
+                            <Profile />
+                        </ProtectedRoute>
+                    } />
+                    <Route path="/leaders" element={
+                        <ProtectedRoute>
+                            <Leaders />
+                        </ProtectedRoute>
+                    } />
+                    <Route path="/match/hotseat" element={
+                        <ProtectedRoute>
+                            <MatchHotseat />
+                        </ProtectedRoute>
+                    } />
+                    <Route path="/match/:matchId" element={
+                        <ProtectedRoute>
+                            <Match />
+                        </ProtectedRoute>
+                    } />
+                </Routes>
+            </AuthProvider>
+        </BrowserRouter>
     </StrictMode>
 )
